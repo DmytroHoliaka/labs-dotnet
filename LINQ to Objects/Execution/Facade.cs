bdc8aacae893508b7dd0ea2c0709c9ec8dc7094c
HoliakaDmytro <holidim39@gmail.com>
2024-03-04 11:03:44 +0200

    Execution: Create a convenient way to execute the program

---
+++ LINQ to Objects/Execution/Facade.cs
@@ -0,0 +1,38 @@
+﻿using LINQ_to_Objects.Inserters;
+using LINQ_to_Objects.Service;
+
+namespace LINQ_to_Objects.Execution
+{
+    internal class Facade
+    {
+        public static void Execute()
+        {
+            DataContext data = new();
+
+            AddressesInserter.InsertAddresses(data.Addresses);
+            CaretakersInserter.InsertCaretakers(data.Caretakers);
+            DetailResultsInserter.InsertDetailResults(data.DetailResults);
+            HorsesInserter.InsertHorses(data.Horses);
+            JockeysInserter.InsertJockeys(data.Jockeys);
+            ParticipantsInserter.InsertParticipants(data.Participants);
+            RacesInserter.InsertRaces(data.Races);
+            StadiumsInserter.InsertStadiums(data.Stadiums);
+
+            var addresses = data.Addresses.Select(a => new
+            {
+                a.Id,
+                a.Country,
+                a.Area,
+                a.District,
+                a.PostalCode,
+                a.Street,
+                a.BuildingNumber,
+            });
+
+            foreach (var address in addresses)
+            {
+                Console.WriteLine(address);
+            }
+        }
+    }
+}
---
+++ LINQ to Objects/Execution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_to_Objects.Execution
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Facade.Execute();
+        }
+    }
+}
