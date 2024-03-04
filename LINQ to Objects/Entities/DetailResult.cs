7ee7c45cc4d983fb88a957b18a77ad467526c846
HoliakaDmytro <holidim39@gmail.com>
2024-03-04 10:59:38 +0200

    Entities: Add DetailResult entity

---
+++ LINQ to Objects/Entities/DetailResult.cs
@@ -0,0 +1,12 @@
+﻿namespace LINQ_to_Objects.Entities
+{
+    internal class DetailResult
+    {
+        public int Id { get; set; }
+        public int Place { get; set; }
+        public int Duration { get; set; }
+        public double MaxSpeed { get; set; }
+
+        public int ParticipantId { get; set; }
+    }
+}
