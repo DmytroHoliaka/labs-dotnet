d064e4a93f9d060be5b25067b5a4fecfc5730fb8
HoliakaDmytro <holidim39@gmail.com>
2024-03-04 10:59:31 +0200

    Entities: Add Caretaker entity

---
+++ LINQ to Objects/Entities/Caretaker.cs
@@ -0,0 +1,15 @@
+﻿namespace LINQ_to_Objects.Entities
+{
+    internal class Caretaker
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? SecondName { get; set; }
+        public string? MiddleName { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public int Salary { get; set; }
+        public string? Responsibility { get; set; }
+        
+        public IEnumerable<int>? HourseIds { get; set; }
+    }
+}
