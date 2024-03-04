9b6e9e668336c365fad4bfd5c0095c0e417a4199
HoliakaDmytro <holidim39@gmail.com>
2024-03-04 07:40:20 +0200

    First lab: Create project

---
+++ LINQ to Objects/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_to_Objects
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
