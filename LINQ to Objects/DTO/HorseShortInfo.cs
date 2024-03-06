3dcbb491230f51d57fbc947b1baa62f720e7bbaf
HoliakaDmytro <holidim39@gmail.com>
2024-03-06 11:01:38 +0200

    DTO: Adding a class to represent brief information about horses

---
+++ LINQ to Objects/DTO/HorseShortInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_Objects.DTO
+{
+    internal class HorseShortInfo
+    {
+        public int Id { get; set; }
+        public string? Nickname { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}.{Nickname} - {Age} year(s)";
+        }
+    }
+}
