f1b0e5d1477960f9d2feb077425b1275516d2abf
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:16:29 +0200

    Entities: Create detail result entity

---
+++ Resources/LINQ to XML/Entities/DetailResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace LINQ_to_XML.Entities
+{
+    [Serializable]
+    public class DetailResult
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+        public int Place { get; set; }
+        public int Duration { get; set; }
+        public double MaxSpeed { get; set; }
+
+        public int ParticipantId { get; set; }
+    }
+}
