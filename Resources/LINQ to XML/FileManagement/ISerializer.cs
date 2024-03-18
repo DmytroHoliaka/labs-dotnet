cd645c31e946e3ad74f58b2341bcbce524039627
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:18:10 +0200

    FileManagement: Create an interface for serializatoin

---
+++ Resources/LINQ to XML/FileManagement/ISerializer.cs
@@ -0,0 +1,9 @@
+﻿using LINQ_to_XML.DataCollector;
+
+namespace LINQ_to_XML.FileManagement
+{
+    internal interface ISerializer
+    {
+        void Serialize(DataContext content, string path);
+    }
+}
