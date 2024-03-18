78b99c1ac3101493a4751cb45df7e7c71290dab2
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:16:50 +0200

    Entities: Create participant entity

---
+++ Resources/LINQ to XML/Entities/Participant.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace LINQ_to_XML.Entities
+{
+    [Serializable]
+    public class Participant
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+        public int Number { get; set; }
+
+        public int JockeyId { get; set; }
+        public int HorseId { get; set; }
+        public int RaceId { get; set; }
+    }
+}
