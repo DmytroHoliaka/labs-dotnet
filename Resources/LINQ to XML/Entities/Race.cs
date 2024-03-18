c3796c6cc7eb0354f59d59ee4ba66dddc0a327ce
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:16:57 +0200

    Entities: Create race entity

---
+++ Resources/LINQ to XML/Entities/Race.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+
+namespace LINQ_to_XML.Entities
+{
+    [Serializable]
+    public class Race
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public int StadiumId { get; set; }
+    }
+}
