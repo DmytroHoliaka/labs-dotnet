8bdc7fcf31303ae268c1218855c29bb07b52543e
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:19:05 +0200

    Service: Add data printer

---
+++ Resources/LINQ to XML/Service/Printer.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LINQ_to_XML.Service
+{
+    internal class Printer
+    {
+        // ToDo: Write file validator
+        // ToDo: Add ability to choice print or not xml file
+        public static void PrintXmlFile(Action<string> source, string path)
+        {
+            XmlDocument doc = new();
+            doc.Load(path);
+
+            using StringWriter sw = new();
+            doc.Save(sw);
+            source(sw.ToString());
+        }
+    }
+}
