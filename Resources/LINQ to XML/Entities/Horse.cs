b996833f4ef665bcffc3742808e02e7c2d805810
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:16:36 +0200

    Entities: Create horse entity

---
+++ Resources/LINQ to XML/Entities/Horse.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace LINQ_to_XML.Entities
+{
+    [Serializable]
+    public class Horse
+    { 
+        [XmlAttribute]
+        public int Id { get; set; }
+        public string? Breed { get; set; }
+        public string? Nickname { get; set; }
+        public int Age { get; set; }
+
+        [XmlArrayItem("CaretakerId")]
+        public List<int>? CaretakerIds { get; set; }
+    }
+}
