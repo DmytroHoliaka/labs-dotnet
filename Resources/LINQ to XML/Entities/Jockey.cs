bdd4cf7dc7249456680ca61e6ba427d849b4cfb6
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:16:43 +0200

    Entities: Create jockey entity

---
+++ Resources/LINQ to XML/Entities/Jockey.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace LINQ_to_XML.Entities
+{
+    [Serializable]
+    public class Jockey
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? SecondName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? Pseudonym { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public int AddressId { get; set; }
+    }
+}
