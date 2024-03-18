a6103a5544e88061f65dd3f07837699fc7b7c8e9
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:16:06 +0200

    Entities: Create address entity

---
+++ Resources/LINQ to XML/Entities/Address.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace LINQ_to_XML.Entities
+{
+    [Serializable]
+    public class Address
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+        public string? Country { get; set; }
+        public string? City { get; set; }
+        public string? Area { get; set; }
+        public string? District { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Street { get; set; }
+        public int BuildingNumber { get; set; }
+    }
+}
