5b091c0cefd0303a50793ec1f9d869b568700957
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:17:31 +0200

    DataCollector: Create context for combining entities

---
+++ Resources/LINQ to XML/DataCollector/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using LINQ_to_XML.Entities;
+using System.Xml.Serialization;
+
+namespace LINQ_to_XML.DataCollector
+{
+    [XmlRoot("Data")]
+    public class DataContext
+    {
+        public List<Address> Addresses;
+        public List<Caretaker> Caretakers;
+        public List<DetailResult> DetailResults;
+        public List<Horse> Horses;
+        public List<Jockey> Jockeys;
+        public List<Participant> Participants;
+        public List<Race> Races;
+        public List<Stadium> Stadiums;
+
+        public DataContext()
+        {
+            Addresses = [];
+            Caretakers = [];
+            DetailResults = [];
+            Horses = [];
+            Jockeys = [];
+            Participants = [];
+            Races = [];
+            Stadiums = [];
+        }
+    }
+}
