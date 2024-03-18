37495b5844bf73baaa0e733d157780494f2317e5
HoliakaDmytro <holidim39@gmail.com>
2024-03-18 14:16:17 +0200

    Entities: Create caretaker entity

---
+++ Resources/LINQ to XML/Entities/Caretaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace LINQ_to_XML.Entities
+{
+    [Serializable]
+    public class Caretaker
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? SecondName { get; set; }
+        public string? MiddleName { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public int Salary { get; set; }
+        public string? Responsibility { get; set; }
+        
+        public int AddressId { get; set; }
+        [XmlArrayItem("HorseId")]
+        public List<int>? HorseIds { get; set; }
+    }
+}
