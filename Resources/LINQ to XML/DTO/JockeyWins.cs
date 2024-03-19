960eb65580880e133dc304b55cb7b3a7d71c2d4d
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:37:31 +0200

    DTO: Add a class to represent a jockey and his wins count

---
+++ Resources/LINQ to XML/DTO/JockeyWins.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.DTO
+{
+    internal class JockeyWins
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public int WinsCount { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id} | Name: {FullName} | Wins: {WinsCount} time(s)";
+        }
+    }
+}
