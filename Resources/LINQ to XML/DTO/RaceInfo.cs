c123ed2ae0a2a57bc5405ba153b05509b8fafd2c
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:38:26 +0200

    DTO: Add a class to represent a race short info

---
+++ Resources/LINQ to XML/DTO/RaceInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.DTO
+{
+    internal class RaceInfo
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id} | Number: {Number} | Start date: {Date}";
+        }
+    }
+}
