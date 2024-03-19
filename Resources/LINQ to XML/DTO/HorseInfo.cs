1cd3f616002364c594c0c1e6599a5e79a8df06eb
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:36:06 +0200

    DTO: Add a class to represent the horse short info

---
+++ Resources/LINQ to XML/DTO/HorseInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.DTO
+{
+    internal class HorseInfo
+    {
+        public int Id {get;set;}
+        public string? Nickname {get;set;}
+        public string? Breed { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id} | Nickname: {Nickname} | Breed: {Breed}";
+        }
+    }
+}
