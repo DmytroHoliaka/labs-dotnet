768e3ac9676acde460d82d9007081c6666b3e801
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:38:17 +0200

    DTO: Add a class to represent a race and its horse count

---
+++ Resources/LINQ to XML/DTO/RaceHorseCount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.DTO
+{
+    internal class RaceHorseCount
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int HorseCount { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id} | Race number: {Number} | Horse count: {HorseCount}";
+        }
+    }
+}
