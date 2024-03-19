d23b6ddee6f87a91593ddae451adc38d6bb65125
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:37:14 +0200

    DTO: Add a class to represent a jockey and race count

---
+++ Resources/LINQ to XML/DTO/JockeyRaceCount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.DTO
+{
+    internal class JockeyRaceCount
+    {
+        public int Id { get; set; }
+        public int RaceCount { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id} | Race count : {RaceCount}";
+        }
+    }
+}
