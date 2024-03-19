874fb33f7248ddc47bf7a84059884d2e10ebefb2
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:35:20 +0200

    DTO: Add a class to represent the caretaker and the horse breed

---
+++ Resources/LINQ to XML/DTO/CaretakerBreed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.DTO
+{
+    internal class CaretakerBreed
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Breed { get; set; }
+        public int HorseCount { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id} | Caretaker: {FullName} | Breed: {Breed} | Horse count: {HorseCount}";
+        }
+    }
+}
