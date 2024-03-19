a838cd29c9df07e14b0616395a60977f6a2270e5
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:36:20 +0200

    DTO: Add a class to represent the horse and it's speed

---
+++ Resources/LINQ to XML/DTO/HorseSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.DTO
+{
+    internal class HorseSpeed
+    {
+        public int Id { get; set; }
+        public string? Nickname { get; set; }
+        public double MaxSpeed { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id} | Nickname: {Nickname} | Max speeed: {MaxSpeed}";
+        }
+    }
+}
