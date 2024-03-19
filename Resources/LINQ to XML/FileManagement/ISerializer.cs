81864498763ae19aaac2686971f3cb431c5e07ab
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:40:15 +0200

    FileManagement: Add Deserialize method to ISerialize interface

--- Resources/LINQ to XML/FileManagement/ISerializer.cs
+++ Resources/LINQ to XML/FileManagement/ISerializer.cs
@@ -5,5 +5,6 @@ namespace LINQ_to_XML.FileManagement
     internal interface ISerializer
     {
         void Serialize(DataContext content, string path);
+        DataContext Deserialize(string path);
     }
 }
