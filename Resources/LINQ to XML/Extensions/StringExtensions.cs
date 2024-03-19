acb7be9687880e634a994ab446fb26d7ef0a9997
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:39:36 +0200

    Extensions: Add string extentions

---
+++ Resources/LINQ to XML/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.Extensions
+{
+    internal static class StringExtensions
+    {
+        public static int ParseToInt(this string value)
+        {
+            return int.TryParse(value, out int parsed)
+                ? parsed : int.MinValue;
+        }
+    }
+}
