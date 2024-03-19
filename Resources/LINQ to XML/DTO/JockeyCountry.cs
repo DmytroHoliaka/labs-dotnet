f28b19f83c1c3de171490d88b3399a607b4b35bd
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:36:56 +0200

    DTO: Add a class to represent a jockey and his country

---
+++ Resources/LINQ to XML/DTO/JockeyCountry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.DTO
+{
+    internal class JockeyCountry
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id} | Name: {FullName} | Country: {Country}";
+        }
+    }
+}
