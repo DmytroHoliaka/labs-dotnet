01564b87141a887ed9aab0f4af29721ef19a1ccb
HoliakaDmytro <holidim39@gmail.com>
2024-03-19 18:38:40 +0200

    DTO: Add a class to represent a stadium short info

---
+++ Resources/LINQ to XML/DTO/StadiumInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_XML.DTO
+{
+    internal class StadiumInfo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int TrackLength { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id} | Name: {Name} | Track: {TrackLength} meter(s)";
+        }
+    }
+}
