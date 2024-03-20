01c92f05650f3037cd88bea2eb4013e486f92731
HoliakaDmytro <holidim39@gmail.com>
2024-03-20 13:59:43 +0200

    Change classes to non-static and add validation to QueryManager

--- Resources/LINQ to XML/Service/QueryManager.cs
+++ Resources/LINQ to XML/Service/QueryManager.cs
@@ -1,29 +1,38 @@
 ﻿using LINQ_to_XML.DTO;
 using LINQ_to_XML.Entities;
 using LINQ_to_XML.Extensions;
-using System.Diagnostics.Metrics;
-using System.IO;
 using System.Xml.Linq;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LINQ_to_XML.Service
 {
     internal class QueryManager
     {
-        // ToDo: Add line Jockey-Address in Er-diagram
-        // ToDo: Check that you doesn't return null
-        // ToDo: Check null for xDoc and remove "?" in xDoc?.Root
-        // ToDo: Change -1 to int.MinValue or int.MaxValue
-        // ToDo: Make "XDocumentExtension" class in Extensions
-        // ToDo: Add Enumerable.Empty to xDocExtention
-        // ToDo: Make Query 16, 17 ... as from help method
+        private readonly XDocument _xDoc;
+        public event Action<string>? ErrorNotify;
+
+        public QueryManager(XDocument? xDoc)
+        {
+            if (xDoc is null)
+            {
+                throw new ArgumentNullException(nameof(xDoc), "XDocument instance cannot be null");
+            }
+
+            _xDoc = xDoc;
+        }
+
 
         /// <summary>
         /// Query 1. Gets all horses of the specified breed
         /// </summary>
-        public static IEnumerable<HorseInfo> GetHorsesByBreed(XDocument xDoc, string breed)
+        public IEnumerable<HorseInfo> GetHorsesByBreed(string? breed)
         {
-            IEnumerable<HorseInfo>? res = xDoc.Root
+            if (breed is null)
+            {
+                ErrorNotify?.Invoke("Breed cannot be null");
+                return Enumerable.Empty<HorseInfo>();
+            }
+
+            IEnumerable<HorseInfo>? res = _xDoc.Root
                ?.Element("Horses")
                ?.Elements()
                 .Where(e => e.Element("Breed")?.Value == breed)
@@ -41,14 +50,20 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 2. Gets all jockeys from the specified country
         /// </summary>
-        public static IEnumerable<JockeyCountry> GetJockeysFromCountry(XDocument xDoc, string country)
+        public IEnumerable<JockeyCountry> GetJockeysFromCountry(string? country)
         {
-            IEnumerable<XElement>? addresses = xDoc
+            if (country is null)
+            {
+                ErrorNotify?.Invoke("Country cannot be null");
+                return Enumerable.Empty<JockeyCountry>();
+            }
+
+            IEnumerable<XElement>? addresses = _xDoc
                 .Root!
                 .Element("Addresses")!
                 .Elements();
 
-            IEnumerable<JockeyCountry>? res = xDoc
+            IEnumerable<JockeyCountry>? res = _xDoc
                  .Root?.Element("Jockeys")?.Elements()
                  .Join(addresses ?? Enumerable.Empty<XElement>(),
                        j => j.Element("AddressId")?.Value,
@@ -70,9 +85,9 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 3. Gets the stadium with the longest track
         /// </summary>
-        public static StadiumInfo? GetStadiumWithLongestTrack(XDocument xDoc)
+        public StadiumInfo GetStadiumWithLongestTrack()
         {
-            StadiumInfo? res = xDoc
+            StadiumInfo? res = _xDoc
                 .Root?.Element("Stadiums")?.Elements()
                 .Select(se => new StadiumInfo
                 {
@@ -95,11 +110,11 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 4. Gets the jockey who won the most times
         /// </summary>
-        public static JockeyWins GetTopWinningJockey(XDocument xDoc)
+        public JockeyWins GetTopWinningJockey()
         {
-            IEnumerable<Participant>? participants = GetParticipants(xDoc);
-            IEnumerable<DetailResult>? results = GetDetailResults(xDoc);
-            IEnumerable<Jockey>? jockeys = GetJockeys(xDoc);
+            IEnumerable<Participant>? participants = GetParticipants();
+            IEnumerable<DetailResult>? results = GetDetailResults();
+            IEnumerable<Jockey>? jockeys = GetJockeys();
 
             JockeyWins? res = jockeys
                ?.Join(participants ?? Enumerable.Empty<Participant>(),
@@ -143,9 +158,9 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 5. Gets the races that were held in the specified year
         /// </summary>
-        public static IEnumerable<RaceInfo> GetRacesByYear(XDocument xDoc, int year)
+        public IEnumerable<RaceInfo> GetRacesByYear(int year)
         {
-            IEnumerable<RaceInfo>? res = xDoc
+            IEnumerable<RaceInfo>? res = _xDoc
                 .Root?.Element("Races")?.Elements()
                 .Select(re => new RaceInfo
                 {
@@ -161,10 +176,10 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 6. Gets the jockeys who participated in the race more than once
         /// </summary>
-        public static IEnumerable<JockeyRaceCount> GetJockeysWithMultipleRaces(XDocument xDoc)
+        public IEnumerable<JockeyRaceCount> GetJockeysWithMultipleRaces()
         {
             IEnumerable<JockeyRaceCount> res =
-                from p in xDoc?.Root?.Element("Participants")?.Elements() ?? Enumerable.Empty<XElement>()
+                from p in _xDoc.Root?.Element("Participants")?.Elements() ?? Enumerable.Empty<XElement>()
                 group p.Attribute("Id")?.Value by p.Element("JockeyId")?.Value into grouped
                 let raceCount = grouped.Count()
                 where raceCount > 1
@@ -180,10 +195,16 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 7. Gets caretakers who live in the specified city
         /// </summary>
-        public static IEnumerable<CaretakerCity> GetCaretakersByCity(XDocument xDoc, string city)
+        public IEnumerable<CaretakerCity> GetCaretakersByCity(string? city)
         {
-            IEnumerable<Address>? addresses = GetAddresses(xDoc);
-            IEnumerable<Caretaker>? caretakers = GetCaretakers(xDoc);
+            if (city is null)
+            {
+                ErrorNotify?.Invoke("City cannot be null");
+                return Enumerable.Empty<CaretakerCity>();
+            }
+
+            IEnumerable<Address>? addresses = GetAddresses();
+            IEnumerable<Caretaker>? caretakers = GetCaretakers();
 
             IEnumerable<CaretakerCity>? res = caretakers
                ?.Join(addresses ?? Enumerable.Empty<Address>(),
@@ -203,10 +224,16 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 8. Gets caretakers who care for the specified horse breed
         /// </summary>
-        public static IEnumerable<CaretakerBreed> GetCaretakersForBreed(XDocument xDoc, string breed)
+        public IEnumerable<CaretakerBreed> GetCaretakersForBreed(string? breed)
         {
-            IEnumerable<Horse>? horses = GetHorses(xDoc);
-            IEnumerable<Caretaker>? caretakers = GetCaretakers(xDoc);
+            if (breed is null)
+            {
+                ErrorNotify?.Invoke("Breed cannot be null");
+                return Enumerable.Empty<CaretakerBreed>();
+            }
+
+            IEnumerable<Horse>? horses = GetHorses();
+            IEnumerable<Caretaker>? caretakers = GetCaretakers();
 
             IEnumerable<CaretakerBreed>? res = caretakers
                ?.SelectMany(c => c.HorseIds!,
@@ -242,11 +269,11 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 9. Gets the horse that has the maximum speed 
         /// </summary>
-        public static HorseSpeed GetFastestHorse(XDocument xDoc)
+        public HorseSpeed GetFastestHorse()
         {
-            IEnumerable<Horse>? horses = GetHorses(xDoc);
-            IEnumerable<Participant>? participants = GetParticipants(xDoc);
-            IEnumerable<DetailResult>? results = GetDetailResults(xDoc);
+            IEnumerable<Horse>? horses = GetHorses();
+            IEnumerable<Participant>? participants = GetParticipants();
+            IEnumerable<DetailResult>? results = GetDetailResults();
 
             HorseSpeed? res = horses
                ?.Join(participants ?? Enumerable.Empty<Participant>(),
@@ -280,10 +307,10 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 10. Gets Jockeys older than the specified number of years
         /// /// </summary>
-        public static IEnumerable<JockeyAge> GetJockeysByAge(XDocument xDoc, int age)
+        public IEnumerable<JockeyAge> GetJockeysByAge(int age)
         {
             IEnumerable<JockeyAge> res =
-                from je in xDoc?.Root?.Element("Jockeys")?.Elements() ?? Enumerable.Empty<XElement>()
+                from je in _xDoc.Root?.Element("Jockeys")?.Elements() ?? Enumerable.Empty<XElement>()
                 let birthDate = je.ParseElementToDateTime("BirthDate")
                 let currentAge = birthDate != DateTime.MinValue
                     ? Math.Round((DateTime.Now - birthDate).Days / 365.25, 2)
@@ -305,11 +332,17 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 11. Gets Jockeys who have participated in a race on a horse with the specified nickname
         /// </summary>
-        public static IEnumerable<JockeyHorseCount> GetJockeysForHorseNickname(XDocument xDoc, string nickname)
+        public IEnumerable<JockeyHorseCount> GetJockeysForHorseNickname(string? nickname)
         {
-            IEnumerable<Horse> horses = GetHorses(xDoc);
-            IEnumerable<Participant> participants = GetParticipants(xDoc);
-            IEnumerable<Jockey> jockeys = GetJockeys(xDoc);
+            if (nickname is null)
+            {
+                ErrorNotify?.Invoke("Horse nickname cannot be null");
+                return Enumerable.Empty<JockeyHorseCount>();
+            }
+
+            IEnumerable<Horse> horses = GetHorses();
+            IEnumerable<Participant> participants = GetParticipants();
+            IEnumerable<Jockey> jockeys = GetJockeys();
 
             IEnumerable<JockeyHorseCount> res =
                 from h in horses
@@ -332,11 +365,11 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 12. Gets the races where horses older than the specified age participated
         /// </summary>
-        public static IEnumerable<RaceHorseCount> GetRacesWithOlderHorses(XDocument xDoc, int age)
+        public IEnumerable<RaceHorseCount> GetRacesWithOlderHorses(int age)
         {
-            IEnumerable<Horse> horses = GetHorses(xDoc);
-            IEnumerable<Participant> participants = GetParticipants(xDoc);
-            IEnumerable<Race> races = GetRaces(xDoc);
+            IEnumerable<Horse> horses = GetHorses();
+            IEnumerable<Participant> participants = GetParticipants();
+            IEnumerable<Race> races = GetRaces();
 
             IEnumerable<RaceHorseCount> res =
                 from h in horses
@@ -357,10 +390,10 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 13. Receives caretakers who earn more than the specified amount of money
         /// </summary>
-        public static IEnumerable<CaretakerInfo> GetCaretakersByEarnings(XDocument xDoc, int salary)
+        public IEnumerable<CaretakerInfo> GetCaretakersByEarnings(int salary)
         {
             IEnumerable<CaretakerInfo> res =
-                from ce in xDoc.Root?.Element("Caretakers")?.Elements() ?? Enumerable.Empty<XElement>()
+                from ce in _xDoc.Root?.Element("Caretakers")?.Elements() ?? Enumerable.Empty<XElement>()
                 let currentSalary = ce.ParseElementToInt("Salary")
                 where currentSalary > salary
                 select new CaretakerInfo
@@ -379,12 +412,12 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 14. Receives horses that have a vet
         /// </summary>
-        public static IEnumerable<HorseInfo> GetHorsesWithVet(XDocument xDoc)
+        public IEnumerable<HorseInfo> GetHorsesWithVet()
         {
             IEnumerable<HorseInfo> res =
-                from he in xDoc.Root?.Element("Horses")?.Elements() ?? Enumerable.Empty<XElement>()
+                from he in _xDoc.Root?.Element("Horses")?.Elements() ?? Enumerable.Empty<XElement>()
                 from i in he.Element("CaretakerIds")?.Elements() ?? Enumerable.Empty<XElement>()
-                join ce in xDoc.Root?.Element("Caretakers")?.Elements() ?? Enumerable.Empty<XElement>()
+                join ce in _xDoc.Root?.Element("Caretakers")?.Elements() ?? Enumerable.Empty<XElement>()
                     on i.Value equals ce.Attribute("Id")?.Value
                 where ce.Element("Responsibility")?.Value == "Veterinary Care"
                 let Id = he.Attribute("Id")?.Value
@@ -404,15 +437,15 @@ namespace LINQ_to_XML.Service
         /// <summary>
         /// Query 15. Gets the stadium where the absolute speed record was set
         /// </summary>
-        public static StadiumSpeed GetRecordSpeedStadium(XDocument xDoc)
+        public StadiumSpeed GetRecordSpeedStadium()
         {
             IEnumerable<StadiumSpeed> details =
-                from se in xDoc.Root?.Element("Stadiums")?.Elements() ?? Enumerable.Empty<XElement>()
-                join re in xDoc.Root?.Element("Races")?.Elements() ?? Enumerable.Empty<XElement>()
+                from se in _xDoc.Root?.Element("Stadiums")?.Elements() ?? Enumerable.Empty<XElement>()
+                join re in _xDoc.Root?.Element("Races")?.Elements() ?? Enumerable.Empty<XElement>()
                     on se.Attribute("Id")?.Value equals re.Element("StadiumId")?.Value
-                join pe in xDoc.Root?.Element("Participants")?.Elements() ?? Enumerable.Empty<XElement>()
+                join pe in _xDoc.Root?.Element("Participants")?.Elements() ?? Enumerable.Empty<XElement>()
                     on re.Attribute("Id")?.Value equals pe.Element("RaceId")?.Value
-                join de in xDoc.Root?.Element("DetailResults")?.Elements() ?? Enumerable.Empty<XElement>()
+                join de in _xDoc.Root?.Element("DetailResults")?.Elements() ?? Enumerable.Empty<XElement>()
                     on pe.Attribute("Id")?.Value equals de.Element("ParticipantId")?.Value
                 select new StadiumSpeed
                 {
@@ -431,9 +464,12 @@ namespace LINQ_to_XML.Service
             };
         }
 
-        public static IEnumerable<Participant> GetParticipants(XDocument xDoc)
+        /// <summary>
+        /// Query 16. Gets all participants
+        /// </summary>
+        public IEnumerable<Participant> GetParticipants()
         {
-            IEnumerable<Participant>? participants = xDoc
+            IEnumerable<Participant>? participants = _xDoc
                 .Root?.Element("Participants")?.Elements()
                 .Select(e => new Participant
                 {
@@ -447,9 +483,12 @@ namespace LINQ_to_XML.Service
             return participants ?? Enumerable.Empty<Participant>();
         }
 
-        public static IEnumerable<DetailResult> GetDetailResults(XDocument xDoc)
+        /// <summary>
+        /// Query 17. Gets all detail results
+        /// </summary>
+        public IEnumerable<DetailResult> GetDetailResults()
         {
-            IEnumerable<DetailResult>? results = xDoc
+            IEnumerable<DetailResult>? results = _xDoc
                 .Root?.Element("DetailResults")?.Elements()
                 .Select(e => new DetailResult
                 {
@@ -463,9 +502,12 @@ namespace LINQ_to_XML.Service
             return results ?? Enumerable.Empty<DetailResult>();
         }
 
-        public static IEnumerable<Jockey> GetJockeys(XDocument xDoc)
+        /// <summary>
+        /// Query 18. Gets all jockeys
+        /// </summary>
+        public IEnumerable<Jockey> GetJockeys()
         {
-            IEnumerable<Jockey>? jockeys = xDoc
+            IEnumerable<Jockey>? jockeys = _xDoc
                .Root?.Element("Jockeys")?.Elements()
                .Select(e => new Jockey
                {
@@ -481,9 +523,12 @@ namespace LINQ_to_XML.Service
             return jockeys ?? Enumerable.Empty<Jockey>();
         }
 
-        public static IEnumerable<Address> GetAddresses(XDocument xDoc)
+        /// <summary>
+        /// Query 19. Gets all addresses
+        /// </summary>
+        public IEnumerable<Address> GetAddresses()
         {
-            IEnumerable<Address>? addresses = xDoc
+            IEnumerable<Address>? addresses = _xDoc
                .Root?.Element("Addresses")?.Elements()
                .Select(e => new Address
                {
@@ -500,9 +545,12 @@ namespace LINQ_to_XML.Service
             return addresses ?? Enumerable.Empty<Address>();
         }
 
-        public static IEnumerable<Caretaker> GetCaretakers(XDocument xDoc)
+        /// <summary>
+        /// Query 20. Gets all caretakers
+        /// </summary>
+        public IEnumerable<Caretaker> GetCaretakers()
         {
-            IEnumerable<Caretaker>? caretakers = xDoc
+            IEnumerable<Caretaker>? caretakers = _xDoc
                .Root?.Element("Caretakers")?.Elements()
                .Select(e => new Caretaker
                {
@@ -520,9 +568,12 @@ namespace LINQ_to_XML.Service
             return caretakers ?? Enumerable.Empty<Caretaker>();
         }
 
-        public static IEnumerable<Horse> GetHorses(XDocument xDoc)
+        /// <summary>
+        /// Query 21. Gets all horses
+        /// </summary>
+        public IEnumerable<Horse> GetHorses()
         {
-            IEnumerable<Horse>? horses = xDoc
+            IEnumerable<Horse>? horses = _xDoc
                .Root?.Element("Horses")?.Elements()
                .Select(e => new Horse
                {
@@ -536,9 +587,12 @@ namespace LINQ_to_XML.Service
             return horses ?? Enumerable.Empty<Horse>();
         }
 
-        public static IEnumerable<Race> GetRaces(XDocument xDoc)
+        /// <summary>
+        /// Query 22. Gets all races
+        /// </summary>
+        public IEnumerable<Race> GetRaces()
         {
-            IEnumerable<Race>? races = xDoc
+            IEnumerable<Race>? races = _xDoc
                .Root?.Element("Races")?.Elements()
                .Select(e => new Race
                {
@@ -551,18 +605,21 @@ namespace LINQ_to_XML.Service
             return races ?? Enumerable.Empty<Race>();
         }
 
-        public static IEnumerable<Stadium> GetStadiums(XDocument xDoc)
+        /// <summary>
+        /// Query 23. Gets all stadiums
+        /// </summary>
+        public IEnumerable<Stadium> GetStadiums()
         {
-            IEnumerable<Stadium>? stadiums = xDoc
+            IEnumerable<Stadium>? stadiums = _xDoc
                .Root?.Element("Stadiums")?.Elements()
                .Select(e => new Stadium
                {
-                    Id = e.ParseAttributeToInt("Id"),
-                    Name = e.GetElementValue("Name"),
-                    TrackLength = e.ParseElementToInt("TrackLength"),
-                    ParticipantCapacity = e.ParseElementToInt("ParticipantCapacity"),
-                    AudienceCapacity = e.ParseElementToInt("AudienceCapacity"),
-                    AddressId = e.ParseElementToInt("AddressId"),
+                   Id = e.ParseAttributeToInt("Id"),
+                   Name = e.GetElementValue("Name"),
+                   TrackLength = e.ParseElementToInt("TrackLength"),
+                   ParticipantCapacity = e.ParseElementToInt("ParticipantCapacity"),
+                   AudienceCapacity = e.ParseElementToInt("AudienceCapacity"),
+                   AddressId = e.ParseElementToInt("AddressId"),
                });
 
             return stadiums ?? Enumerable.Empty<Stadium>();
