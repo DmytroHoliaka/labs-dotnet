bf02c354f6cba6a769560e1db94b8452c2be75c5
HoliakaDmytro <holidim39@gmail.com>
2024-03-20 13:54:31 +0200

    DTO: Remove unnecessary usings

--- Resources/LINQ to XML/DTO/HorseInfo.cs
+++ Resources/LINQ to XML/DTO/HorseInfo.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LINQ_to_XML.DTO
+﻿namespace LINQ_to_XML.DTO
 {
     internal class HorseInfo
     {
-        public int Id {get;set;}
-        public string? Nickname {get;set;}
+        public int Id { get; set; }
+        public string? Nickname { get; set; }
         public string? Breed { get; set; }
 
         public override string ToString()
--- Resources/LINQ to XML/DTO/JockeyWins.cs
+++ Resources/LINQ to XML/DTO/JockeyWins.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LINQ_to_XML.DTO
+﻿namespace LINQ_to_XML.DTO
 {
     internal class JockeyWins
     {
