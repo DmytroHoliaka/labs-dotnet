08817ba62e559f349297ac0322ca3962f94759c9
HoliakaDmytro <holidim39@gmail.com>
2024-03-20 13:57:51 +0200

    Extensions: Add null validation for XElementExtensions

--- Resources/LINQ to XML/Extensions/XElementExtensions.cs
+++ Resources/LINQ to XML/Extensions/XElementExtensions.cs
@@ -1,44 +1,48 @@
 ﻿using System.Xml.Linq;
 
+#pragma warning disable CS8604
+
 namespace LINQ_to_XML.Extensions
 {
     internal static class XElementExtensions
     {
-        public static int ParseAttributeToInt(this XElement xElement, string tagName)
+        public static int ParseAttributeToInt(this XElement? xElement, string? tagName)
         {
-            return int.TryParse(xElement.Attribute(tagName)?.Value, out int parsed)
+            return int.TryParse(xElement?.Attribute(tagName)?.Value, out int parsed)
                 ? parsed : int.MinValue;
         }
 
-        public static int ParseElementToInt(this XElement xElement, string tagName)
+        public static int ParseElementToInt(this XElement? xElement, string? tagName)
         {
-            return int.TryParse(xElement.Element(tagName)?.Value, out int parsed)
+            return int.TryParse(xElement?.Element(tagName)?.Value, out int parsed)
                 ? parsed : int.MinValue;
         }
 
-        public static double ParseElementToDouble(this XElement xElement, string tagName)
+        public static double ParseElementToDouble(this XElement? xElement, string? tagName)
         {
-            return double.TryParse(xElement.Element(tagName)?.Value, out double parsed)
+            return double.TryParse(xElement?.Element(tagName)?.Value, out double parsed)
                 ? parsed : double.MinValue;
         }
 
-        public static List<int> ParseElementToList(this XElement xElement, string tagName)
+        public static List<int> ParseElementToList(this XElement? xElement, string? tagName)
         {
-            return xElement.Element(tagName)?.Elements()
+            return xElement?.Element(tagName)?.Elements()
                 .Select(e => int.TryParse(e.Value, out int parsed)
                                 ? parsed : int.MinValue)
                 .ToList() ?? [];
         }
 
-        public static DateTime ParseElementToDateTime(this XElement xElement, string tagName)
+        public static DateTime ParseElementToDateTime(this XElement? xElement, string? tagName)
         {
-            return DateTime.TryParse(xElement.Element(tagName)?.Value, out DateTime parsed)
+            return DateTime.TryParse(xElement?.Element(tagName)?.Value, out DateTime parsed)
                 ? parsed : DateTime.MinValue;
         }
 
-        public static string GetElementValue(this XElement xElement, string tagName)
+        public static string GetElementValue(this XElement? xElement, string? tagName)
         {
-            return xElement.Element(tagName)?.Value ?? "<unknown>";
+            return xElement?.Element(tagName)?.Value ?? "<unknown>";
         }
     }
 }
+
+#pragma warning restore CS8604
