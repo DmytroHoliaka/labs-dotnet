576929959399ee28e902f33c5f4d916e624e4d1e
HoliakaDmytro <holidim39@gmail.com>
2024-04-29 16:13:55 +0300

    Entities: Add entities with navigation properties

---
+++ Resources/JSON/Entities/Participant.cs
@@ -0,0 +1,20 @@
+﻿using Json.FileManagement;
+
+namespace Json.Entities
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+
+        public Jockey? Jockey { get; set; }
+        public Horse? Horse { get; set; }
+        public DetailResult? DetailResult { get; set; }
+        public Race? Race { get; set; }
+
+        public override string ToString()
+        {
+            return ObjectSerializer.Serialize(this);
+        }
+    }
+}
---
+++ Resources/JSON/Entities/Stadium.cs
@@ -0,0 +1,21 @@
+﻿using Json.FileManagement;
+
+namespace Json.Entities
+{
+    public class Stadium
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int TrackLength { get; set; }
+        public int ParticipantCapacity { get; set; }
+        public int AudienceCapacity { get; set; }
+
+        public Address? Address { get; set; }
+        public List<Race>? Races { get; set; }
+
+        public override string ToString()
+        {
+            return ObjectSerializer.Serialize(this);
+        }
+    }
+}
