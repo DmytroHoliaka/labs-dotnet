cf66f62e35c3449a0f8aab3769a648540eef83eb
HoliakaDmytro <holidim39@gmail.com>
2024-04-29 16:16:39 +0300

    Service: Added service classes

---
+++ Resources/JSON/Service/JsonSerializerOptionsWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using JSON.Converters;
+
+namespace Json.Service;
+
+public static class JsonSerializerOptionsWrapper
+{
+    public static JsonSerializerOptions GetPreserveOptions()
+    {
+        return new JsonSerializerOptions()
+        {
+            ReferenceHandler = ReferenceHandler.Preserve,
+            WriteIndented = true,
+        };
+    }
+
+    public static JsonSerializerOptions GetMinimizedDetailResultsOptions()
+    {
+        return new JsonSerializerOptions()
+        {
+            Converters = { new DetailResultConverter() },
+            WriteIndented = true,
+        };
+    }
+    
+    public static JsonSerializerOptions GetMinimizedCaretakersOptions()
+    {
+        return new JsonSerializerOptions()
+        {
+            Converters = { new CaretakerConverter() },
+            WriteIndented = true,
+        };
+    }
+}
---
+++ Resources/JSON/Service/Printer.cs
@@ -0,0 +1,16 @@
+﻿namespace Json.Service;
+
+public static class Printer
+{
+    public static void PrintMenu()
+    {
+        Console.WriteLine(
+            """
+            ---------- Application menu ----------
+            1. Serialize the data context;
+            2. Deserialize the data context;
+            3. Find the maximum speed on the race (JsonDocument);
+            4. Find the sum of all the salaries of the caretakers (JsonNode).
+            """);
+    }
+}
