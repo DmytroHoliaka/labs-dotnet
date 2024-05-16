d4251d31de6935d4b15a9a38edae2a397fe9f351
HoliakaDmytro <holidim39@gmail.com>
2024-05-16 11:40:39 +0300

    Enumerations: Add main enums to project

---
+++ Resources/CreationalPatterns/Enumerations/ConnectionInterface.cs
@@ -0,0 +1,9 @@
+namespace CreationalPatterns.Enumerations;
+
+public enum ConnectionInterface
+{
+    Sata,
+    Nvme,
+    Pcie,
+    Unknown,
+}
---
+++ Resources/CreationalPatterns/Enumerations/SocketType.cs
@@ -0,0 +1,11 @@
+namespace CreationalPatterns.Enumerations;
+
+public enum SocketType
+{
+    Lga1151,
+    Am4,
+    Tr4,
+    Am5,
+    Lga1200,
+    Unknown,
+}
