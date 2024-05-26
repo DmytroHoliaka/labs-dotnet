545ee81308442f58215cba711751fd521b007920
HoliakaDmytro <holidim39@gmail.com>
2024-05-26 14:59:49 +0300

    Sixth lab: Create project

---
+++ Resources/BehavioralPatterns/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BehavioralPatterns
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
